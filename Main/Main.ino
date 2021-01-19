862a2e354fb94643c6c8c6e24adaf069c08aca83
sennevanpelt <45144481+sennevanpelt@users.noreply.github.com>
2021-01-19 12:33:26 +0100

    Mac adress uitlezen en printen

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,16 @@
+//Includes
+#include <WiFi.h>
+
+//Variabels
+
+
 
 void setup()
 {
   Serial.begin(9600); // starts the serial port at 9600
+  delay(100);
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
 }
  
 void loop()
