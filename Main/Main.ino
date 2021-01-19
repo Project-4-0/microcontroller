4ad5000c202f99be6d0bd2110c0a625e2dbfa0a0
sennevanpelt <45144481+sennevanpelt@users.noreply.github.com>
2021-01-19 14:00:08 +0100

    Geef foutmelding bij foutive waarde

--- Main/Main.ino
+++ Main/Main.ino
@@ -13,6 +13,11 @@ int Temperatuur_Pin = 36;
 int Grondvochtigeheid_Pin = 39;
 int Ldr_Pin = 34;
 
+//Max waardes Sensoren
+int Max_Temperatuur_Sensor = 4000;
+int Max_Grondvochtigheid_Sensor = 4000;
+int Max_Ldr_Sensor = 4000;
+
 
 void setup()
 {
@@ -30,25 +35,52 @@ void loop()
     //Request Variable_Box_Id API
     Variable_Box_Id = 15; 
     Serial.println("Nieuwe Variable_Box_Id toegekend");  
+    
     //Testen Meting Sensoren
     Ldr_Sensor = analogRead(Ldr_Pin);
     Grondvochtigheid_Sensor = analogRead(Grondvochtigeheid_Pin);
     Temperatuur_Sensor = analogRead(Temperatuur_Pin);
-    // Als er fout is Print fout
+    
+    // Als sensor defect is(ERROR)
     if(Ldr_Sensor == 0){
-      Serial.println("Error Ldr_Sensor"); 
+      Serial.println("Error Ldr_Sensor Kapot"); 
     }
+    
     if(Temperatuur_Sensor == 0){
-      Serial.println("Error Temperatuur_Sensor"); 
+      Serial.println("Error Temperatuur_Sensor Kapot"); 
     }
+    
     if(Grondvochtigheid_Sensor == 0){
-      Serial.println("Error Grondvochtigheid_Sensor"); 
+      Serial.println("Error Grondvochtigheid_Sensor Kapot"); 
     }
-
   }
-  Serial.println("geslaagd");
-  Serial.println(Temperatuur_Sensor);
-  Serial.println(Grondvochtigheid_Sensor);
-  Serial.println(Ldr_Sensor); 
-  delay(1000);
+
+  //Na opstart connectie met de API
+  Ldr_Sensor = analogRead(Ldr_Pin);
+  Grondvochtigheid_Sensor = analogRead(Grondvochtigeheid_Pin);
+  Temperatuur_Sensor = analogRead(Temperatuur_Pin);
+
+  //Foute waarde gemeten (ERROR)
+  if(Ldr_Sensor > Max_Ldr_Sensor){
+    Serial.println("Error Ldr_Sensor Foute waarde"); 
+  }
+  
+  if(Temperatuur_Sensor > Max_Temperatuur_Sensor){
+    Serial.println("Error Temperatuur_Sensor Foute waarde"); 
+  }
+  
+  if(Grondvochtigheid_Sensor > Max_Grondvochtigheid_Sensor){
+    Serial.println("Error Grondvochtigheid_Sensor Foute waarde"); 
+  }
+  
+  
+
+
+  //Debugging
+//  Serial.println("Debugging");
+//  Serial.println(Temperatuur_Sensor);
+//  Serial.println(Grondvochtigheid_Sensor);
+//  Serial.println(Ldr_Sensor);
+//  Serial.println("End Debugging"); 
+//  delay(1000);
 }
