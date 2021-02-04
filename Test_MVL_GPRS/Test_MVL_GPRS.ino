5715b9dd19124f0494ad34555a951d13a4967486
sennevanpelt <45144481+sennevanpelt@users.noreply.github.com>
2021-02-04 14:38:25 +0100

    Create Test_MVL_GPRS.ino

---
+++ Test_MVL_GPRS/Test_MVL_GPRS.ino
@@ -0,0 +1,221 @@
+// TTGO T-Call pin definitions
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+// Increase RX buffer if needed
+#define TINY_GSM_RX_BUFFER 1024
+
+#include <TinyGsmClient.h>
+
+#include <Wire.h>
+#include <ArduinoHttpClient.h>
+
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+
+bool setPowerBoostKeepOn(int en)
+{
+  Wire.beginTransmission(IP5306_ADDR);
+  Wire.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    Wire.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    Wire.write(0x35); // 0x37 is default reg value
+  }
+  return Wire.endTransmission() == 0;
+}
+
+// Increase RX buffer if needed
+#define TINY_GSM_RX_BUFFER 1024
+
+
+
+// Uncomment this if you want to see all AT commands
+//#define DUMP_AT_COMMANDS
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define SerialAT Serial1
+// or Software Serial on Uno, Nano
+//#include <SoftwareSerial.h>
+//SoftwareSerial SerialAT(2, 3); // RX, TX
+
+#define STATUS_CHECK_INTERVAL     60000L
+unsigned long checkstatus_timeout = 0;
+bool connectionStatus=false;
+
+// Your GPRS credentials
+// Leave empty, if missing user or pass
+/// PUT YOUR GSM / GPRS RELATED ///
+const char apn[]  = "web.be";
+const char gprsUser[] = "web";
+const char gprsPass[] = "web";
+
+// Server details
+const char server[] = "vsh.pp.ua";
+const char resource[] = "/TinyGSM/logo.txt";
+const int  port = 80;
+
+int ReCnctFlag;  // Reconnection Flag
+int ReCnctCount = 0;  // Reconnection counter
+
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+TinyGsmClient client(modem);
+HttpClient http(client, server, port);
+
+void setup() {
+  // Set console baud rate
+  SerialMon.begin(115200);
+  delay(10);
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+  bool   isOk = setPowerBoostKeepOn(1);
+  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+  
+// Set-up modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+  // Set GSM module baud rate
+  ///SerialAT.begin(115200);
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println(F("Initializing modem..."));
+  modem.restart();
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print(F("Modem: "));
+  SerialMon.println(modemInfo);
+
+  // Unlock your SIM card with a PIN
+  modem.simUnlock("1111");
+
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork(240000L)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" OK");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+  SerialMon.print(F("Connecting to APN: "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" OK");
+
+}
+
+void HTTPClientHandle(void){
+  if(!connectionStatus){
+     SerialMon.print(F("One time connection. Waiting for network..."));
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    connectionStatus=false;
+    delay(3000);
+    return;
+  }
+  SerialMon.println(" OK");
+
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    connectionStatus=false;
+    delay(3000);
+    return;
+  }
+  SerialMon.println(" OK");
+  connectionStatus=true;
+}
+
+  SerialMon.print(F("Performing HTTP GET request... "));
+  int err = http.get(resource);
+  if (err != 0) {
+    SerialMon.println(F("failed to connect"));
+    delay(5000);
+    return;
+  }
+
+  int status = http.responseStatusCode();
+  SerialMon.println(status);
+  if (!status) {
+    delay(5000);
+    return;
+  }
+/* /// this is for informational reasons only not needed ///
+  while (http.headerAvailable()) {
+    String headerName = http.readHeaderName();
+    String headerValue = http.readHeaderValue();
+   //SerialMon.println(headerName + " : " + headerValue);
+  }
+*/
+  int length = http.contentLength();
+  if (length >= 0) {
+    SerialMon.print(F("Content length is: "));
+    SerialMon.println(length);
+  }
+  if (http.isResponseChunked()) {
+    SerialMon.println(F("The response is chunked"));
+  }
+
+  String body = http.responseBody();
+  SerialMon.println(F("Response:"));
+  SerialMon.println(body);
+
+  SerialMon.print(F("Body length is: "));
+  SerialMon.println(body.length());
+
+  // Shutdown
+
+  http.stop();
+  SerialMon.println(F("Server disconnected"));
+
+  ///modem.gprsDisconnect(); /// leave GPRS connected ///
+  ///SerialMon.println(F("GPRS disconnected"));
+  SerialMon.println(F("GPRS is NOT disconnected"));
+}
+
+
+void loop(){
+
+  Serial.println("text");
+  delay(1000);
+  
+}
